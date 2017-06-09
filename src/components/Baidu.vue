9bd0e13ca9151ea1001aed0416a61016bea0803c
cjd <459953103@qq.com>
2017-06-09 20:37:54 +0800

    1

---
+++ src/components/Baidu.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button class="primary circular">
+            <i class="on-center">mail</i>
+        </button>
+        <div class="card">
+            <div class="card-content">
+                Card Content
+            </div>
+        </div>
+        <label>
+            <q-checkbox v-model="checked"></q-checkbox>
+            Checkbox Label
+        </label>
+        <q-chips v-model="model" placeholder="Type some names"></q-chips>
+        <q-datetime v-model="date" type="date"></q-datetime>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                checked: true,
+                model: ["421"],
+                date:""
+            }
+        }
+    }
+
+</script>
---
+++ src/components/Index.vue
@@ -0,0 +1,54 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+  
+  export default {
+    data() {
+      return {
+      
+      }
+    },
+    computed: {
+      position() {
+      
+      }
+    },
+    methods: {
+     
+    },
+    mounted() {
+      
+    },
+    beforeDestroy() {
+      if (this.orienting) {
+        window.removeEventListener('deviceorientation', this.orient, false)
+      }
+      else if (this.rotating) {
+        window.removeEventListener('devicemove', this.rotate, false)
+      }
+      else {
+        document.removeEventListener('mousemove', this.move)
+      }
+    }
+  }
+
+</script>
+
+<style lang="stylus">
+  .logo-container {
+    width 192px;
+    height 268px;
+    perspective 800px;
+    position absolute;
+    top 50%;
+    left 50%;
+    transform translateX(-50%) translateY(-50%);
+  }
+  
+  .logo {
+    position absolute;
+    transform-style preserve-3d;
+  }
+</style>
